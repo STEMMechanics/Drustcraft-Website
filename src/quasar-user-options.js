9aa12f65fc62a779f56550917aac636744f27c36
James Collins <james.collins@outlook.com.au>
2022-08-02 21:40:26 +1000

    updated quasar config

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -1,10 +1,16 @@
 
 import './styles/quasar.scss'
 import '@quasar/extras/material-icons/material-icons.css'
+import { LocalStorage, SessionStorage, Loading } from 'quasar'
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
+  config: {
+    loading: { }
+  },
   plugins: {
+    LocalStorage,
+    SessionStorage,
+    Loading
   }
 }
