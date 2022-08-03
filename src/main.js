30fec4b9e00d89cb01363cf2c6d35f99b9c1fda2
James Collins <james.collins@outlook.com.au>
2022-08-03 12:42:43 +1000

    added main.scss

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import { createApp } from 'vue'
+import { Vue, createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import { Quasar } from 'quasar'
+import './main.scss'
 import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
+createApp(App)
+    .use(Quasar, quasarUserOptions)
+    .use(store)
+    .use(router)
+    .mount('#app')
